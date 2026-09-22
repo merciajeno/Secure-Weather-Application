9f57bf5d72bdce22b289d0f46b74aaad32f8379c
Mercia <mercia.jeno@gmail.com>
2026-09-22 15:29:19 +0530

    added audit table and all the repository layer

--- src/main/java/com/mercia/weather/entities/Audit.java
+++ src/main/java/com/mercia/weather/entities/Audit.java
@@ -2,37 +2,47 @@ package com.mercia.weather.entities;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="audits")
+@Table(name = "audit")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Audit {
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	private Long cityId;
-	
-	private Long userId;
-	
-	private Action action;
-	
-	private String endpoint;
-	
-	private Status status;
-	
-	private LocalDateTime timestamp;
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long cityId;
+
+    @Enumerated(EnumType.STRING)
+    private Action action;
+
+    private String endpoint;
+
+    private String httpMethod;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime timestamp;
+
+    @Column(columnDefinition = "jsonb")
+    private String metadata;
 }
---
+++ src/main/java/com/mercia/weather/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.mercia.weather.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mercia.weather.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
